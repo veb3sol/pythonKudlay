210b92b956c2998b95e5aef954e70fb2b4146def
Ruslan <veb3solidity@gmail.com>
2024-01-30 11:40:26 +0200

    less24

--- less24.py
+++ less24.py
@@ -14,8 +14,25 @@
 
 produkt = {'title': 'Sony', 'price': 100}
 
+#print(produkt.items())  #dict_items([('title', 'Sony'), ('price', 100)]) - список кортежей ключ-значение
+#print(produkt.keys())  #dict_keys(['title', 'price']) - список ключей
+
+#print(produkt.pop('title')) # удалит запись по ключу, вернет значение ключа. если такого ключа нету - ошибка
+#print(produkt.pop('title', 'no')) # если такого ключа нету - no
+#print(produkt) #{'price': 100}
+
+#print(produkt.popitem()) # убирает любую запись???
+#print(produkt)  #{'title': 'Sony'}  - осталась одна запись
+
+#print(produkt.setdefault('title'))  #Sony  и словарь не поменялся
+#print(produkt.setdefault('title2'))  #такого ключа нету, он создался в словаре со значением None
+#print(produkt.setdefault('title3', 'test'))  #такого ключа нету, он создался в словаре со значением test
+
 produkt.update({'tema': 'babki'})
 print(produkt)      # {'title': 'Sony', 'price': 100, 'tema': 'babki'} - добавился ключ-значение
+produkt.update({'price': 200})   #{'title': 'Sony', 'price': 100, 'tema': 'babki'}
 
 produkt.update({'price':200})
 print(produkt)      #{'title': 'Sony', 'price': 200, 'tema': 'babki'}   - перезаписался
+
+print(produkt.values())     #dict_values(['Sony', 200, 'babki']) -- список значений
