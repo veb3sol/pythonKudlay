c126262f629b475f6b066a9697dade4284307120
Ruslan <veb3solidity@gmail.com>
2024-02-16 15:06:19 +0200

    oss1

---
+++ oss.py
@@ -0,0 +1,9 @@
+import os
+
+# print(os.getcwd()) #C:\Users\Ruslan_WORK\PycharmProjects\pythonKudlay - путь к файлу
+# print(os.name) # nt - названи ос
+# print(os.environ) #словарь, который содержит текущие переменные окружения
+print(os.environ['PATH']) # строка с путями среды окружения -- пути к прогам которые доступны с ком строки везде
+# добавляем путь к путям среды окружения
+
+# C:\Users\Ruslan_WORK\PycharmProjects\pythonKudlay\venv\Scripts;E:\KURSY\PYTHON\SQL\db\bin;C:\Program Files (x86)\Common Files\Intel\Shared Libraries\redist\intel64\compiler;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Windows\system32;C:\Program Files\wget;C:\Program Files\dotnet\;C:\Program Files\Microsoft SQL Server\130\Tools\Binn\;C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\;C:\flutter\bin;"C:\Program Files\Git\bin\git.exe;C:\Windows\System32";C:\Program Files\Git\cmd;C:\ProgramData\ComposerSetup\bin;C:\Program Files\Azure Data Studio\bin;C:\xampp\php;C:\Program Files\nodejs\;C:\python385\Scripts\;C:\python385\;C:\Users\Ruslan_WORK\AppData\Local\Microsoft\WindowsApps;C:\Users\Ruslan_WORK\AppData\Local\Programs\Microsoft VS Code\bin;C:\Program Files\JetBrains\PyCharm Community Edition 2020.2.1\bin;;C:\Users\Ruslan_WORK\.dotnet\tools;C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2020.3.2\bin;;"C:\Program Files\Git\bin\git.exe;C:\Program Files\Git\cmd;C:\Windows\System32";C:\flutter\bin;C:\Program Files\Azure Data Studio\bin;C:\Users\Ruslan_WORK\AppData\Roaming\Composer\vendor\bin;C:\xampp\php;C:\Program Files\Azure Data Studio\bin;C:\Users\Ruslan_WORK\AppData\Roaming\npm
